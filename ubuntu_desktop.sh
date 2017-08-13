c653d7cf69c57e442e47705dfe3aa592275c956a
Angad Singh <angad@angad.sg>
2017-08-13 22:59:35 +0800

    updated desktop

--- ubuntu_desktop.sh
+++ ubuntu_desktop.sh
@@ -1,61 +1,45 @@
-#apt-get update
-#apt-get -y install software-properties-common
-#add-apt-repository ppa:alexmurray/indicator-sensors
-#add-apt-repository ppa:caffeine-developers/ppa
-#add-apt-repository ppa:zeal-developers/ppa
-#add-apt-repository ppa:saiarcot895/myppa
-#apt-get update
-#apt-get -y install apt-fast
-#apt-fast install -y git \
-                    #htop \
-                    #smartmontools \
-                    #lm-sensors \
-                    #powertop \
-                    #indicator-multiload \
-                    #indicator-cpufreq \
-                    #indicator-sensors \
-                    #caffeine \
-                    #openssh-server \
-                    #zsh \
-                    #byobu \
-                    #screen \
-                    #python-pip
-                    #vim \
-                    #vim-gnome \
-                    #wireshark \
-                    #software-center \
-                    #gimp \
-                    #atom \
-                    #vlc \
-                    #calibre \
-                    #midori \
-                    #glipper \
-                    #openssl \
-                    #libcurl4-openssl-dev \
-                    #libxml2 \
-                    #libssl-dev \
-                    #libxml2-dev \
-                    #pinentry-curses \
-                    #xclip \
-                    #libffi-dev \
-                    #python-dev \
-                    #axel \
-                    #silversearch-ag \
-                    #cpufrequtils \
-                    #zeal \
-                    #unity-control-center \
-                    #gnome-color-manager \
-                    #atom \
-                    #gstreamer1.0-libav \ # H264 decoder for midori browser
-                    #nmap \
-                    #whois \
-                    #spotify-client \
-                    #gnome-color-manager \
-                    #iostat \
-#apt-fast -y upgrade
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' >> /etc/apt/sources.list
+add-apt-repository ppa:saiarcot895/myppa
 apt-get update
-apt-fast install docker-engine
-
-
+apt-get -y install apt-fast
+apt-fast install -y git \
+                    htop \
+                    smartmontools \
+                    lm-sensors \
+                    powertop \
+                    indicator-multiload \
+                    indicator-cpufreq \
+                    caffeine \
+                    openssh-server \
+                    zsh \
+                    byobu \
+                    screen \
+                    python-pip
+                    vim \
+                    vim-gnome \
+                    wireshark \
+                    software-center \
+                    gimp \
+                    vlc \
+                    calibre \
+                    midori \
+                    glipper \
+                    openssl \
+                    libcurl4-openssl-dev \
+                    libxml2 \
+                    libssl-dev \
+                    libxml2-dev \
+                    pinentry-curses \
+                    xclip \
+                    libffi-dev \
+                    python-dev \
+                    axel \
+                    silversearch-ag \
+                    cpufrequtils \
+                    zeal \
+                    unity-control-center \
+                    gstreamer1.0-libav \ # H264 decoder for midori browser
+                    nmap \
+                    whois \
+                    spotify-client \
+                    gnome-color-manager 
+apt-fast -y upgrade
