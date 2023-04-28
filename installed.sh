d8e80efc34284f5cbacde2816084fd3129b28877
Angad Singh <angad@angad.sg>
2023-04-28 20:52:33 +0800

    added from hawk machine

--- installed.sh
+++ installed.sh
@@ -1,3 +1,4 @@
-apt list --installed | cut -f1 -d'/' > tmp_installed
-paste tmp_installed installed | sort | uniq > installed
-rm tmp_installed
+apt list --installed | cut -f1 -d'/' > tmp1
+cat tmp1 installed > tmp2
+sed -re 's/\s+$//' tmp2 | sort | uniq > installed
+rm tmp1 tmp2
